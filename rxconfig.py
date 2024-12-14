0dc482d523fb5aa47449daeba4b506f374a8b582
Walkercito <yoanky2005@gmail.com>
2024-12-14 20:58:38 +0000

    fix typo

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Hackathon_AlcadiaNET",
-    api_url = "https://fastapi-backend-ndo6.onrender.com"
+    api_url = "https://fastapi-backend-ndo6.onrender.com/ws"
 )
